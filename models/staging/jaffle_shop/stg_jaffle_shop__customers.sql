aa39ac0a53a2c2cbb15a98a97f737a19ea0bdcd2
Diana Castillo <diana.castillorebolledo@tcs.com>
2025-07-28 02:02:17 +0000

    comments and testing

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-from {{ source('jaffle_shop', 'customers') }}
+from {{ source('jaffle_shop', 'customers') }} /* source('source_name', 'object_name') */
